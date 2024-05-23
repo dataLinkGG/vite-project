7a7ae41e2ebbbfd261a8911b67bbc5f502a9533a
Thomas Cionek <thomascionek97@gmail.com>
2024-05-23 20:04:23 -0300

    Add basic layout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 function App() {
-  return <div>Test</div>;
+  return (
+    <>
+      <header className="masthead clear">
+        <div className="centered">
+          <div className="site-branding">
+            <h1 className="site-title">Components Playground</h1>
+          </div>
+        </div>
+      </header>
+      <main className="main-area">Main Area</main>
+      <footer className="footer-area">
+        <div className="centered"></div>
+      </footer>
+    </>
+  );
 }
 
 export default App;
